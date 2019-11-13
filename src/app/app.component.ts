a353819d6d68a0b8824e53f6c8ca61ae972af813
nikfrankson <nikfrankson@mail.fresnostate.edu>
2019-11-12 23:12:45 -0800

    Lab Assignment 7

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ToastService } from './toast/toast.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'toast-app';
+  toastTypes: Array<string> = [];
+  constructor(private toastService: ToastService) {
+    this.toastTypes = ['success', 'info', 'warning', 'danger']
+  }
+
+ // success, info, warning, danger
+  showToast() {
+
+    const rand = Math.floor(Math.random() * 4);
+    console.log('My random number is: ' + rand);
+    const toastType = this.toastTypes[rand];
+    const toastMessage = "Hi! This is a message my random number is: " + rand;
+    const duration = 4000;
+    this.toastService.showToast(toastType, toastMessage, duration);
+  }
+
+
+
+}
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ToastModule} from './toast/toast.module';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ToastModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
